ca855f42ff117198b34cb5ce71d1a543fb2a778e
ravitejajavvadi <javvadiraviteja@gmail.com>
2020-03-08 22:59:51 +0530

    add the largestsmallestnumberinarray class and modify reverse word class

---
+++ src/largestSmallestNUmberInArray.java
@@ -0,0 +1,10 @@
+
+public class largestSmallestNUmberInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("largestSmallestNUmberInArray");
+	}
+
+}
--- src/reverseWord.java
+++ src/reverseWord.java
@@ -4,8 +4,15 @@ public class reverseWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String inp="....suvarna.....teechnosoft";
-		
+		String inp=".....suvarna.....technosoft";
+		int len= inp.length();
+		String s= inp.substring(0, 4);
+		String s1= inp.substring(5, 12);
+		String s2= inp.substring(17, 27);
+		System.out.println("inp string lenth is :"+len+"  "+"string s1:"+s1+ "   "+ "string s1 :"+s2);
+	    StringBuffer r= new StringBuffer(s1);
+	    StringBuffer r1= new StringBuffer(s2);
+		System.out.println("string reverse is:"+s+r.reverse()+s+r1.reverse());
 	}
 
 }
